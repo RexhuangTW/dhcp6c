4c3397400686d28c331353627bc7716337f0b288
RexhuangTW <nestike.huang@gmail.com>
2017-11-19 19:29:45 +0800

    modify

--- src/dhcp6.c
+++ src/dhcp6.c
@@ -9,43 +9,43 @@
 
 #include "dhcp6c.h"
 
-#define	DST_PORT 547
-#define	SRC_PORT 546
-#define	DHCPV6_MUTICAST_ADDRESS	"ff02::1:2"
+#define DST_PORT 547
+#define SRC_PORT 546
+#define DHCPV6_MUTICAST_ADDRESS "ff02::1:2"
 
 int add_option(uint8_t *dest, enum option_code opt_code, uint16_t len, uint8_t *data)
 {
-	struct dhcpv6_option *opt_packet = (struct dhcpv6_option*)dest;
+	struct dhcpv6_option *opt_packet = (struct dhcpv6_option *)dest;
 
 	opt_packet->option = htons(opt_code);
 	opt_packet->len = htons(len);
 	memcpy(opt_packet->value, data, len);
 
-	return(sizeof(struct dhcpv6_option) + len);
+	return (sizeof(struct dhcpv6_option) + len);
 }
 int add_message(uint8_t *dest, enum message_type msg_type, uint16_t len, uint8_t *data)
 {
-	struct dhcpv6_soliocit *opt_packet = (struct dhcpv6_soliocit*)dest;
+	struct dhcpv6_soliocit *opt_packet = (struct dhcpv6_soliocit *)dest;
 
 	opt_packet->message_type = msg_type;
 	memcpy(opt_packet->trans_ID, data, len);
 
-	return(sizeof(struct dhcpv6_soliocit) + len);	
+	return (sizeof(struct dhcpv6_soliocit) + len);
 }
 int add_domain_name_packet(uint8_t *dest, uint8_t option, uint8_t len, uint8_t *name)
 {
-	struct dhcpv6_domain_name *do_name = (struct dhcpv6_domain_name*)dest;
+	struct dhcpv6_domain_name *do_name = (struct dhcpv6_domain_name *)dest;
 
 	do_name->option = option;
 	do_name->len = len;
 	memcpy(do_name->name, name, len);
 
-	return(sizeof(struct dhcpv6_domain_name) + len);
+	return (sizeof(struct dhcpv6_domain_name) + len);
 }
 void int2hex(uint8_t *dest, int num)
 {
-	dest[1] = num & 0xFF;		// low byte
-	dest[0] = (num >> 8) & 0xFF;	// high byte
+	dest[1] = num & 0xFF;		 // low byte
+	dest[0] = (num >> 8) & 0xFF; // high byte
 }
 
 int main(int argc, char *argv[])
@@ -53,20 +53,21 @@ int main(int argc, char *argv[])
 	struct sockaddr_in6 addr = {0};
 	struct sockaddr_in6 client_addr = {0};
 	struct in_addr ipaddr = {0};
-	struct dhcpv6_IA_ID ia_id_v;
+	struct dhcpv6_IA_ID ia_id_v = {0};
 	int fd, cnt;
 	uint8_t buf[1024] = {0};
 	uint8_t sub_value[521] = {0};
-	size_t offest=0, sub_offest=0;
+	size_t offest = 0, sub_offest = 0;
 	uint8_t soliocit[3] = {0xAA, 0xAA, 0xAA};
 	uint8_t duid[18] = {0x00, 0x04, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
-			0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10};
+						0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10};
 	uint8_t req_opt[10] = {0x00, 0x17, 0x00, 0x18, 0x00, 0x17, 0x00, 0x18, 0x00, 0x01};
 	uint8_t elapsed_time[2];
-	uint8_t client_name[8]={'R','e','x','_','t','e','s','t'};
+	uint8_t *client_name = "Rex_test";
+	uint8_t *vend_class_data = "MSFT 5.0";
 
 	/* create what looks like an ordinary UDP socket */
-	if(( fd = socket(AF_INET6, SOCK_DGRAM, 0))<0)
+	if ((fd = socket(AF_INET6, SOCK_DGRAM, 0)) < 0)
 	{
 		perror("socket");
 		exit(1);
@@ -81,11 +82,11 @@ int main(int argc, char *argv[])
 	int val = 1;
 	setsockopt(fd, IPPROTO_IPV6, IPV6_V6ONLY, &val, sizeof(val));
 	setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));
-	setsockopt(fd, IPPROTO_IPV6, IPV6_RECVPKTINFO, &val, sizeof(val));	
+	setsockopt(fd, IPPROTO_IPV6, IPV6_RECVPKTINFO, &val, sizeof(val));
 	client_addr.sin6_family = AF_INET6;
 	client_addr.sin6_port = htons(SRC_PORT);
 
-	if(bind(fd, (struct sockaddr*)&client_addr, sizeof(client_addr))<0)
+	if (bind(fd, (struct sockaddr *)&client_addr, sizeof(client_addr)) < 0)
 	{
 		perror("bind");
 		exit(1);
@@ -97,31 +98,39 @@ int main(int argc, char *argv[])
 
 	/* Elapsedtime */
 	int2hex(elapsed_time, 300);
-	offest += add_option( buf + offest, OPTION_ELAPSED_TIME, sizeof(elapsed_time), elapsed_time);
-	
+	offest += add_option(buf + offest, OPTION_ELAPSED_TIME, sizeof(elapsed_time), elapsed_time);
+
 	/* Client Identifier */
-	offest += add_option( buf + offest, OPTION_CLIENTID, sizeof(duid), duid);
+	offest += add_option(buf + offest, OPTION_CLIENTID, sizeof(duid), duid);
 
 	/* IA_NA */
-	ia_id_v.iaid = 0x79e99408;
+	ia_id_v.iaid = htonl(0x0894e979);
 	ia_id_v.t1 = htons(0);
 	ia_id_v.t2 = htons(0);
-	offest += add_option( buf + offest, OPTION_IA_NA, sizeof(ia_id_v),(uint8_t *)&ia_id_v);
-	
-	/*Fully Qualified Domain Name*/
-	sub_offest += add_domain_name_packet(sub_value, 0x00,sizeof(client_name), client_name);
-	offest += add_option( buf + offest, OPTION_CLIENT_FQDN, sub_offest, sub_value);
-	
-	/*Option Request*/
-	offest += add_option( buf + offest, OPTION_ORO, sizeof(req_opt), req_opt);
+	offest += add_option(buf + offest, OPTION_IA_NA, sizeof(ia_id_v), (uint8_t *)&ia_id_v);
 
-	if(sendto( fd, buf, offest, 0, (struct sockaddr*)&addr, sizeof(addr))<0)
+	/* Fully Qualified Domain Name */
+	sub_offest += add_domain_name_packet(sub_value, 0x00, strlen(client_name), client_name);
+	offest += add_option(buf + offest, OPTION_CLIENT_FQDN, sub_offest, sub_value);
+
+	/* Vendor Class */
+	struct dhcpv6_vendor_class *vend_class = malloc(sizeof(struct dhcpv6_vendor_class) + strlen(vend_class_data));
+	vend_class->enterprise_id = htonl(311);
+	vend_class->len = strlen(vend_class_data);
+	memcpy(vend_class->data, vend_class_data, vend_class->len);
+
+	offest += add_option(buf + offest, OPTION_VENDOR_CLASS, sizeof(struct dhcpv6_vendor_class) + strlen(vend_class_data), vend_class);
+
+	/* Option Request */
+	offest += add_option(buf + offest, OPTION_ORO, sizeof(req_opt), req_opt);
+
+	if (sendto(fd, buf, offest, 0, (struct sockaddr *)&addr, sizeof(addr)) < 0)
 	{
 		perror("sendto");
 		exit(1);
 	}
 
+	free(vend_class);
+
 	return 0;
 }
-
-
--- src/dhcp6c.h
+++ src/dhcp6c.h
@@ -1,3 +1,9 @@
+
+#pragma once
+#include <stdint.h>
+#include <stdbool.h>
+#include <netinet/in.h>
+
 enum message_type
 {
 	SOLICIT = 1,
@@ -6,11 +12,11 @@ enum message_type
 	CONFIRM = 4
 };
 
-enum  option_code
+enum option_code
 {
 	OPTION_CLIENTID = 1,
 	OPTION_SERVERID = 2,
-	OPTION_IA_NA = 3, 
+	OPTION_IA_NA = 3,
 	OPTION_IA_TA = 4,
 	OPTION_IAADDR = 5,
 	OPTION_ORO = 6,
@@ -20,18 +26,18 @@ enum  option_code
 	OPTION_AUTH = 11,
 	OPTION_UNICAST = 12,
 	OPTION_STATUS_CODE = 13,
-	OPTION_RAPID_COMMIT = 14 ,
+	OPTION_RAPID_COMMIT = 14,
 	OPTION_USER_CLASS = 15,
-	OPTION_VENDOR_CLASS = 16,	
+	OPTION_VENDOR_CLASS = 16,
 	OPTION_VENDOR_OPTS = 17,
 	OPTION_INTERFACE_ID = 18,
-	OPTION_RECONF_MSG= 19,
-	OPTION_RECONF_ACCEP= 20,
+	OPTION_RECONF_MSG = 19,
+	OPTION_RECONF_ACCEP = 20,
 	OPTION_SIP_SERVER_D = 21,
 	OPTION_SIP_SERVER_A = 22,
 	OPTION_DNS_SERVERS = 23,
 	OPTION_DOMAIN_LIST = 24,
-	OPTION_IA_PD= 25,
+	OPTION_IA_PD = 25,
 	OPTION_IAPREFIX = 26,
 	OPTION_NIS_SERVERS = 27,
 	OPTION_NISP_SERVERS = 28,
@@ -44,30 +50,38 @@ enum  option_code
 	OPTION_GEOCONF_CIVIC = 36,
 	OPTION_REMOTE_ID = 37,
 	OPTION_SUBSCRIBER_ID = 38,
- 	OPTION_CLIENT_FQDN = 39,
+	OPTION_CLIENT_FQDN = 39,
 	OPTION_PANA_AGENT = 40
 };
 
 struct dhcpv6_soliocit
 {
-	uint8_t		message_type;
-	uint8_t		trans_ID[0];
+	uint8_t message_type;
+	uint8_t trans_ID[0];
 };
 struct dhcpv6_option
 {
-	uint16_t	option;
-	uint16_t	len;
-	uint8_t		value[0];
+	uint16_t option;
+	uint16_t len;
+	uint8_t value[0];
 };
 struct dhcpv6_domain_name
 {
-	uint8_t		option;
-	uint8_t		len;
-	uint8_t		name[0];
+	uint8_t option;
+	uint8_t len;
+	uint8_t name[0];
 };
 struct dhcpv6_IA_ID
 {
-    uint32_t    iaid;
-    uint32_t    t1;
-    uint32_t    t2;
+	uint32_t iaid;
+	uint32_t t1;
+	uint32_t t2;
 };
+
+struct dhcpv6_vendor_class
+{
+	uint32_t enterprise_id;
+	uint16_t len;
+	uint8_t data[0];
+} __attribute__((packed));
+;
