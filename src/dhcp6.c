8fa0c9b0c85aefe0768436c3a2c34980ff6667f8
rex_huang <rex_huang@gemteks.com>
2017-11-20 10:35:38 +0800

    modify

--- src/dhcp6.c
+++ src/dhcp6.c
@@ -6,12 +6,14 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
 
 #include "dhcp6c.h"
 
-#define DST_PORT 547
-#define SRC_PORT 546
+#define DST_PORT 546
+#define SRC_PORT 547
 #define DHCPV6_MUTICAST_ADDRESS "ff02::1:2"
+
 
 int add_option(uint8_t *dest, enum option_code opt_code, uint16_t len, uint8_t *data)
 {
@@ -54,17 +56,22 @@ int main(int argc, char *argv[])
 	struct sockaddr_in6 client_addr = {0};
 	struct in_addr ipaddr = {0};
 	struct dhcpv6_IA_ID ia_id_v = {0};
-	int fd, cnt;
+	int fd, cnt,i;
 	uint8_t buf[1024] = {0};
 	uint8_t sub_value[521] = {0};
 	size_t offest = 0, sub_offest = 0;
-	uint8_t soliocit[3] = {0xAA, 0xAA, 0xAA};
+	uint8_t soliocit[3] = {0};
 	uint8_t duid[18] = {0x00, 0x04, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
 						0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10};
-	uint8_t req_opt[10] = {0x00, 0x17, 0x00, 0x18, 0x00, 0x17, 0x00, 0x18, 0x00, 0x01};
+	uint8_t req_opt[4] = {24,23,17,39};
 	uint8_t elapsed_time[2];
 	uint8_t *client_name = "Rex_test";
 	uint8_t *vend_class_data = "MSFT 5.0";
+	int urandom_fd = -1;
+	uint32_t iaid = 0x0894e979;
+	uint32_t enterprise_id = 311;
+	uint16_t t1 = 0, t2 = 0;
+	int delay_time = 300;
 
 	/* create what looks like an ordinary UDP socket */
 	if ((fd = socket(AF_INET6, SOCK_DGRAM, 0)) < 0)
@@ -92,21 +99,24 @@ int main(int argc, char *argv[])
 		exit(1);
 	}
 	/* Add message type */
+	urandom_fd = open("/dev/urandom", O_RDONLY);
+	read(urandom_fd, soliocit, sizeof(soliocit));
+	close(urandom_fd);
 	offest += add_message(buf + offest, SOLICIT, sizeof(soliocit), soliocit);
 
 	/* Addoption */
 
 	/* Elapsedtime */
-	int2hex(elapsed_time, 300);
+	int2hex(elapsed_time, delay_time);
 	offest += add_option(buf + offest, OPTION_ELAPSED_TIME, sizeof(elapsed_time), elapsed_time);
 
 	/* Client Identifier */
 	offest += add_option(buf + offest, OPTION_CLIENTID, sizeof(duid), duid);
 
 	/* IA_NA */
-	ia_id_v.iaid = htonl(0x0894e979);
-	ia_id_v.t1 = htons(0);
-	ia_id_v.t2 = htons(0);
+	ia_id_v.iaid = htonl(iaid);
+	ia_id_v.t1 = htons(t1);
+	ia_id_v.t2 = htons(t2);
 	offest += add_option(buf + offest, OPTION_IA_NA, sizeof(ia_id_v), (uint8_t *)&ia_id_v);
 
 	/* Fully Qualified Domain Name */
@@ -115,15 +125,22 @@ int main(int argc, char *argv[])
 
 	/* Vendor Class */
 	struct dhcpv6_vendor_class *vend_class = malloc(sizeof(struct dhcpv6_vendor_class) + strlen(vend_class_data));
-	vend_class->enterprise_id = htonl(311);
+	vend_class->enterprise_id = htonl(enterprise_id);
 	vend_class->len = strlen(vend_class_data);
 	memcpy(vend_class->data, vend_class_data, vend_class->len);
-
 	offest += add_option(buf + offest, OPTION_VENDOR_CLASS, sizeof(struct dhcpv6_vendor_class) + strlen(vend_class_data), vend_class);
 
 	/* Option Request */
-	offest += add_option(buf + offest, OPTION_ORO, sizeof(req_opt), req_opt);
+	memset(sub_value,0x00,sizeof(sub_value));
+	for(i =0; i<sizeof(req_opt); i++)
+	{
+		sub_value[i*2] = (req_opt[i]>>8) & 0xFF;
+		sub_value[i*2+1] = req_opt[i] & 0xFF;
+	}
 
+	offest += add_option(buf + offest, OPTION_ORO, sizeof(req_opt)*2, sub_value);
+
+	
 	if (sendto(fd, buf, offest, 0, (struct sockaddr *)&addr, sizeof(addr)) < 0)
 	{
 		perror("sendto");
